c842a7a42c2446e706b7514aa6d4151291b7f06b
nicolasfueyo <nicolas93100.fueyo@gmail.com>
2021-01-27 22:40:16 +0100

    Inscription et connexion OK

--- _FICHIERS/cvven.sql
+++ _FICHIERS/cvven.sql
@@ -1,15 +1,9 @@
--- *********************************************************
--- CECI EST LA VERSION MySQL, on utilisera ceci jusqu'à
--- ce qu'on arrive à configurer correctement vers PostgreSQL
--- Params de BD : voir app/config/database.php
--- *********************************************************
-
 -- phpMyAdmin SQL Dump
 -- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : Dim 24 jan. 2021 à 21:39
+-- Généré le : mer. 27 jan. 2021 à 22:39
 -- Version du serveur :  10.4.17-MariaDB
 -- Version de PHP : 7.4.13
 
@@ -37,7 +31,7 @@ CREATE TABLE `utilisateur` (
   `id` int(11) NOT NULL,
   `nom` varchar(32) DEFAULT NULL,
   `email` varchar(256) DEFAULT NULL,
-  `mdp` varchar(32) DEFAULT NULL,
+  `mdp` varchar(256) DEFAULT NULL,
   `prenom` varchar(32) DEFAULT NULL,
   `tel` varchar(20) DEFAULT NULL,
   `role` varchar(10) DEFAULT NULL,
@@ -49,7 +43,7 @@ CREATE TABLE `utilisateur` (
 --
 
 INSERT INTO `utilisateur` (`id`, `nom`, `email`, `mdp`, `prenom`, `tel`, `role`, `adresse`) VALUES
-(1, 'FUEYO RODRIGUEZ', 'nicolas93100.fueyo@gmail.com', '$2y$10$4RZhDAsaQPxIABwBcKA2feNNK', 'Nicolas', '123456', 'CLIENT', 'blabla@gmail.com');
+(2, 'FUEYO RODRIGUEZ', 'lala@gmail.com', '$2y$10$pt0o.20oLOkWZNIl7WIVkeKLkXI.xa2DK5ySnM59bYaSUogwE9Jzu', 'Nicolas', '12122121', 'CLIENT', '9');
 
 --
 -- Index pour les tables déchargées
@@ -71,7 +65,7 @@ ALTER TABLE `utilisateur`
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
