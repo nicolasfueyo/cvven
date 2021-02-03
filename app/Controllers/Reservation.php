b4c0d24d287207855b704d047c406142eb8bf424
nicolasfueyo <nicolas93100.fueyo@gmail.com>
2021-02-03 21:58:35 +0100

    Formulaire de réservation en cours..

--- app/Controllers/Reservation.php
+++ app/Controllers/Reservation.php
@@ -1,11 +1,23 @@
 <?php namespace App\Controllers;
 
+use App\Models\TypeLogementModel;
 use CodeIgniter\Controller;
 
 class Reservation extends Controller {
     public function index() {
-        helper(['form']);
+
         $data = [];
+
+        // Charge types de logement
+        $model = new TypeLogementModel();
+        $typesLogements = [];
+        foreach ( $model->findAll() as $typeLogement ){
+            $typesLogements[ $typeLogement['id'] ] = $typeLogement['nom'];
+        }
+        $data['typesLogements'] = $typesLogements;
+
+        helper(['form']);
+
         echo view('reservation', $data);
     }
 }
--- app/Views/reservation.php
+++ app/Views/reservation.php
@@ -92,7 +92,7 @@
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Type de logement</label>
-                        <select class="form-control"></select>
+                        <?php echo form_dropdown('logement', $typesLogements, null, ['class'=>'form-control']) ?>
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Nombre de logements</label>
